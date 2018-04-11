d82b80c0ce799d7c1147c6cb689b475204324090
Kamil Machański <>
2018-04-11 17:37:52 +0200

    zadania zarobione kolokwium

--- kolo1/src/main/java/zadanie3/Zadanie3.java
+++ kolo1/src/main/java/zadanie3/Zadanie3.java
@@ -6,43 +6,46 @@
 package zadanie3;
 
 import java.util.Scanner;
+import jdk.nashorn.internal.ir.BreakNode;
 
 /**
  *
  * @author student
  */
 public class Zadanie3 {
-    
+
     Scanner input = new Scanner(System.in);
-    
+
     private int[] tab = new int[10];
-    
-    public void wprLiczbe()
-    {
-        System.out.println("Chcesz wpisać do którego indeksu: [od 0 do 9]");
-        int i = input.nextInt();
-        
-        System.out.println("Podaj liczbę do wpisania: ");
-        int n = input.nextInt();
-        tab[i]=n;
+
+    public void wprLiczbe() {
+        int a = 1;
+        while (a == 1) {
+            System.out.println("Chcesz wpisać do którego indeksu: [od 0 do 9](wpisz 11 aby zakonczyc wpisywanie");
+            int i = input.nextInt();
+            if (i == 11) {
+                a = 0;
+            } else {
+
+                System.out.println("Podaj liczbę do wpisania: ");
+                int n = input.nextInt();
+                tab[i] = n;
+            }
+        }
     }
-    
-    public  void zawTablicy()
-    {
-        for(int i=0; i<tab.length; i++)
-        {
+
+    public void zawTablicy() {
+        for (int i = 0; i < tab.length; i++) {
             System.out.println(tab[i]);
         }
     }
-    
-    public  void srZawTablicy()
-    {
+
+    public void srZawTablicy() {
         int suma = 0;
-        for(int i=0; i<tab.length; i++)
-        {
+        for (int i = 0; i < tab.length; i++) {
             suma = suma + tab[i];
         }
-        System.out.println("Srednia: "+(suma/tab.length)); 
+        System.out.println("Srednia: " + (suma / tab.length));
     }
-    
+
 }
